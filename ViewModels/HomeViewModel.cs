023a341bca319c50ec2818bfda7670b8aa63ec53
KacperMazurIT <kacper.mazur.it@gmail.com>
2020-11-20 22:43:48 +0100

    Add project files.

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RemoteBakery.Models;
+
+namespace RemoteBakery.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}
---
+++ ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RemoteBakery.Models;
+
+namespace RemoteBakery.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
